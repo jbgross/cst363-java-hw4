34da1f52055bc9972e270ac29ad5f72a543b685a
jbgross <gross.joshua.b@gmail.com>
2024-10-29 11:42:30 -0700

    completed SelectQuery code and updated .gitignore to ignore database files (*.db) created by the project code

--- src/heapdb/query/SelectQuery.java
+++ src/heapdb/query/SelectQuery.java
@@ -46,21 +46,42 @@ public class SelectQuery  {
 		Table result = new Table(resultSchema);
 		ArrayList<String> joinColumns  = new ArrayList<>();
 		// TODO  find the list of join column of the 2 tables
+		Schema s1 = table1.getSchema();
+		Schema s2 = table2.getSchema();
+		for(int i = 0; i < s1.size(); i++) {
+			for(int k = 0; k < s2.size(); k++) {
+				if(s1.getName(i).equals(s2.getName(k))) {
+					joinColumns.add(s1.getName(i));
+					break;
+				}
+			}
+		}
 		for (Tuple t1: table1) {
 			for (Tuple t2: table2) {
-				// TODO if t1 matches all join columns of t2
-				// TODO then insert the join of t1 and t2 into 
-				//      the result table
+				boolean isMatch = true;
+				for(String column : joinColumns) {
+					if(! t1.get(column).equals(t2.get(column))) {
+						isMatch = false;
+						break;
+					}
+				}
+				if(isMatch) {
+					Tuple joinTuple = Tuple.joinTuple(resultSchema, t1, t2);
+					result.insert(joinTuple);
+				}
 			}
 		}
 		return result;
 	}
 	
 	public ITable eval(ITable table) {
-		Schema s;
+		Schema s = table.getSchema();
+		boolean newSchema = false;
 		// if there is a projection operation, make the new schema
-		if (colNames==null) s = table.getSchema();
-		else s = table.getSchema().project(colNames);
+		if (colNames != null) {
+			s = table.getSchema().project(colNames);
+			newSchema = true;
+		}
 		
 		Table result = new Table(s);
 		for (Tuple t: table) { 
